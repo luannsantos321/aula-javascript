99776c3fdc7b84eb1b999cf571a9827e96a1ba2a
luannsantos321 <luannsantostec@gmail.com>
2022-06-12 14:51:21 -0300

    primeira aula de JavaScript completa

---
+++ main.js
@@ -0,0 +1,45 @@
+//var nome = "Rackel "
+//alert(nome + "Te amo!");
+//var frase = "Japão é o melhor time do mundo"
+
+//console.log(frase.replace("Japão, Brasil"))
+//alert(frase.replace("Japão", "Brasil"));
+//console.log(nome.toUpperCase());
+
+//var lista = ["maçã","pêra", "laranja"];
+//lista.push("uva");
+//console.log(lista.toString());
+
+//var fruta = [{nome: "maça", cor: "vermelha"}, {nome:"uva", cor:"roxa"}]
+//console.log(fruta);
+//alert(fruta[1].cor);
+
+/*
+var idade = prompt("Qual sua idade?");
+
+if (idade >= 18){
+    alert("maior de idade!");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count = count + 1;
+};
+*/ 
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+
+    alert(count);
+};
+*/
+
+var d = new Date();
+alert(d);
